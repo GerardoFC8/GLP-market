0cccef3fac6ebc2b0bc124027fb6469225e786ab
Jamir Vargas <73477811+Jamir-boop@users.noreply.github.com>
2021-09-17 00:51:45 -0500

    avance sub_barra

--- resources/views/layout/sub_barra_navegacion.blade.php
+++ resources/views/layout/sub_barra_navegacion.blade.php
@@ -3,28 +3,33 @@
         <nav class="nav2">
             <ul class="menu-box-nav2">
                 <li class="li-nav2">
-                    <button role="link" onclick="window.location='#'" class="navbutton-nav2">
-                        Mantenimiento
+                    <button role="link" onclick="window.location='/productos?filtro=LLANTAS+Y+AROS'" class="navbutton-nav2">
+                        <img src="image/sub_rueda.png" alt="sub imagen"><br>
+                        Ruedas / Aros
                     </button>
                 </li>
                 <li class="li-nav2">
-                    <button role="link" onclick="window.location='#'" class="navbutton-nav2">
-                        Consultas
+                    <button role="link" onclick="window.location='/productos?filtro=BATERIAS'" class="navbutton-nav2">
+                        <img src="image/sub_bateria.png" alt="sub imagen"><br>
+                        Baterias
                     </button>
                 </li>
                 <li class="li-nav2">
-                    <button role="link" onclick="window.location='#'" class="navbutton-nav2">
-                        Reparaciones
+                    <button role="link" onclick="window.location='/productos?filtro=AUDIO+Y+VIDEO'" class="navbutton-nav2">
+                        <img src="image/sub_sonido.png" alt="sub imagen"><br>
+                        Audio y Video
                     </button>
                 </li>
                 <li class="li-nav2">
-                    <button role="link" onclick="window.location='#'" class="navbutton-nav2">
-                        Gas
+                    <button role="link" onclick="window.location='/productos?filtro=LIMPIEZA'" class="navbutton-nav2">
+                        <img src="image/sub_limpieza.png" alt="sub imagen"><br>
+                        Limpieza
                     </button>
                 </li>
                 <li class="li-nav2">
-                    <button role="link" onclick="window.location='#'" class="navbutton-nav2">
-                        Otros
+                    <button role="link" onclick="window.location='/productos?filtro=Herramientas'" class="navbutton-nav2">
+                        <img src="image/sub_herr.png" alt="sub imagen"><br>
+                        Herramientas
                     </button>
                 </li>
             </ul>
