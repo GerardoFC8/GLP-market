1f1fd6eed09c5b8c20d076bc83bf30dff909cb1d
Jamir Vargas <73477811+Jamir-boop@users.noreply.github.com>
2021-09-28 17:40:34 -0500

    ligthbox productos

--- resources/views/productos.blade.php
+++ resources/views/productos.blade.php
@@ -34,9 +34,9 @@
                         <img src="{{asset('storage').'/'.$product->Imagen_producto}}" alt="unidad producto">
                     </div>
                     <div class="box-contenido">
-                        <h3>{{ $product->Nombre }}</h3>
-                        <p>{{ $product->Descripcion }}</p>
-                        <h2>S/.{{ $product->Precio }}</h2>
+                        <h3 class="showligthbox" id="<?=$product->id?>">{{ $product->Nombre }}</h3>
+                        <p class="showligthbox" id="<?=$product->id?>">{{ $product->Descripcion }}</p>
+                        <h2 class="showligthbox" id="<?=$product->id?>">S/.{{ $product->Precio }}</h2>
 
                         <form action="" method="post">
                             @csrf
@@ -45,23 +45,24 @@
                             
                             <input type="number" name="cantidad" value="1">
                             <input type="submit" value="Agregar" name="btnAgregar" class="btnAgre">
-
                         </form>
                     </div>
-
-                    <div class="ligthbox<?=$product->id?>">
-                        <img src="{{asset('storage').'/'.$product->Imagen_producto}}" alt="imagen producto ligthbox">
+                    
+                    <div class="ligthbox_container">
+                        <div class="ligthbox<?=$product->id?> ligthbox" style="visibility: hidden;">
+                            <img src="{{asset('storage').'/'.$product->Imagen_producto}}" alt="imagen producto ligthbox">
+                        </div>
                     </div>
 
                     <script>
-                        const imagen1 = document.querySelector(".ligthbox<?=$product->id?>");
-                        imagen1.style.visibility = "hidden";
+                        document.querySelector(".ligthbox<?=$product->id?>").addEventListener('click', () =>{
+                            document.querySelector(".ligthbox<?=$product->id?>").style.visibility = "hidden";
+                        });
                     </script>
                 </div>
             </div>
         @endforeach
     </div>
-
 </section>
 
 
@@ -77,6 +78,18 @@
         echo "<script>alert('Producto $producto agregado con exito al carrito de compras');</script>";
     }
 ?>
+
+<script>
+    const botones = document.querySelectorAll(".showligthbox");
+
+    botones.forEach((item) => {
+        item.addEventListener("click", () => {
+            document.querySelector(".ligthbox"+item.id).style.visibility = "visible";
+            console.log("click in"+item.id);
+        })
+    });
+</script>
+
 
 @include('layout.footer')
 @endsection
