c5955a1eae8aaa466d9a5bd1a719af1440821650
Jamir Vargas <73477811+Jamir-boop@users.noreply.github.com>
2021-09-28 14:47:56 -0500

    mantenimiento added

--- resources/views/productos.blade.php
+++ resources/views/productos.blade.php
@@ -43,54 +43,25 @@
                             <input type="hidden" name="producto" value="{{ $product->Nombre }}">
                             <input type="hidden" name="Precio" value="{{ $product->Precio }}">
                             
-                            <div class="botones_cantidad">
-                                <div class="boton_disminuir_cant">-</div>
-                                <input type="number" name="cantidad" value="1" class="cantidad_cant">
-                                <div class="boton_aumentar_cant">+</div>
-                            </div>
+                            <input type="number" name="cantidad" value="1">
                             <input type="submit" value="Agregar" name="btnAgregar" class="btnAgre">
 
                         </form>
-                        <a href="{{ url('add-cart/'.$product->id) }}" class="agregar_carro">
-                            <div class="texto_agregar">
-                                <span>Agregar al carrito</span>
-                            </div>
-                        </a>
                     </div>
+
+                    <div class="ligthbox">
+                        <img src="{{asset('storage').'/'.$product->Imagen_producto}}" alt="imagen producto ligthbox">
+                    </div>
+
+                    <script>
+                        console.log("<?=$product->Nombre?>");
+                    </script>
                 </div>
             </div>
         @endforeach
     </div>
 
 </section>
-
-<script>
-    let cantidad_stock = 10;
-
-    const disminuir_btn = document.querySelector('.botones_cantidad .boton_disminuir_cant');
-    const aumentar_btn = document.querySelector('.botones_cantidad .boton_aumentar_cant');
-    let num_cantidad_stock = document.querySelector('.botones_cantidad .cantidad_cant');
-    
-    console.log(disminuir_btn, aumentar_btn, num_cantidad_stock);
-
-    disminuir_btn.addEventListener('click', () => {
-        console.log("-");
-        if(num_cantidad_stock.innerHTML <= 0){
-            num_cantidad_stock.value = 0;
-        }else{
-            num_cantidad_stock.value = parseInt(num_cantidad_stock.value) - 1;
-        }
-    });
-
-    aumentar_btn.addEventListener('click', () => {
-        console.log("+");
-        if(num_cantidad_stock.value >= cantidad_stock){
-            num_cantidad_stock.value = cantidad_stock;
-        }else{
-            num_cantidad_stock.value = parseInt(num_cantidad_stock.value) + 1;
-        }
-    });
-</script>
 
 
 <?php 
